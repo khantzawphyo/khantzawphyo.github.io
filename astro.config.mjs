d75df17d052eaf11289c3dc54225d90479fce240
Khant Zaw Phyo <khantzawphyo02@gmail.com>
2026-04-21 22:53:14 +0630

    Add site config for GitHub Pages user site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://khantzawphyo.github.io",
   redirects: {
     "/resume": "/khantzawphyo_cv.pdf",
   },
